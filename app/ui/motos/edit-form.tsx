c32eea05950b2326c9478b0e78941e7f9094153f
Weslley Oliveira <weslleyzera2020@gmail.com>
2024-06-18 17:53:43 +0100

    correcao do edti foi retirado alguns componentes

--- app/ui/motos/edit-form.tsx
+++ app/ui/motos/edit-form.tsx
@@ -1,12 +1,6 @@
 'use client';
 
 import { CustomerField, Vehicle } from '@/app/lib/definitions';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateVehicle } from '@/app/lib/actions';
