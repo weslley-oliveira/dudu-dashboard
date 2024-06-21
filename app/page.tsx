fad1f5e362c19e99c862320665fe88a49ed0d772
Weslley Oliveira <weslleyzera2020@gmail.com>
2024-06-21 16:10:53 +0100

    primeira pagina

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,13 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Bem-vindo ao MotoManage.</strong>{' '}
+            <strong>Bem-vindo ao MotoManage</strong>{' '}
+          </p>
+          <p
+            className={`text-l text-gray-800`}
+          >
             Esta solução robusta é projetada para simplificar a gestão de oficinas de motos, garantindo operações eficientes e suaves.
           </p>
 
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,7 +4,7 @@ import { FaMotorcycle } from 'react-icons/fa';
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={` flex flex-row items-center leading-none text-white`}
     >
       <div className="text-4xl text-white items-center flex gap-2">
         <FaMotorcycle className='text-6xl'/> MotoManage
