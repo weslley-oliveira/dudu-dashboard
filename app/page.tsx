2481e97f3ce0832e083f4b5486d924f5512330f5
Weslley Oliveira <weslleyzera2020@gmail.com>
2024-06-21 12:27:27 +0100

    mucanca foto

--- app/page.tsx
+++ app/page.tsx
@@ -31,14 +31,14 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-alexandre.jpeg"
+            src="/hero-desktop.png"
             width={1000}
             height={760}
             alt="Screenshots of the dashboard project showing desktop version"
             className="hidden md:block"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/hero-desktop.png"
             width={560}
             height={620}
             alt="Screenshot of the dashboard project showing mobile version"
