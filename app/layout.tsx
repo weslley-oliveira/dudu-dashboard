64dd5053158ee748505539346348a17c40f92769
Weslley Oliveira <weslleyzera2020@gmail.com>
2024-06-26 16:42:28 +0100

    portview

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
