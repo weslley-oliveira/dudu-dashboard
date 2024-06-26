c4d6bf8d57b4241d1bda03af725f0559302d4487
Weslley Oliveira <weslleyzera2020@gmail.com>
2024-06-26 17:33:09 +0100

    tste

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex grow overflow-hidden">
+        <div className="grow p-6 md:overflow-y-auto md:p-12">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
