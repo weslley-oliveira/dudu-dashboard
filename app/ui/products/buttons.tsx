85614d3f3a0ee3bbdc4ea4e44942a09dce0fabd1
Natan Barboza <natanbarboza084@gmail.com>
2024-07-15 17:01:12 -0300

    changing button color

--- app/ui/products/buttons.tsx
+++ app/ui/products/buttons.tsx
@@ -6,7 +6,7 @@ export function CreateProduct() {
   return (
     <Link
       href="/dashboard/inventory/products/create"
-      className="flex h-10 items-center rounded-lg bg-green-600 px-4 text-sm font-medium text-white transition-colors hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
       <span className="hidden md:block">Create Product</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
